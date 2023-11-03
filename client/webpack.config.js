e4c92b93c51023d7c8b1425bf92ba01e4a370dc0
astrunk87 <134554632+astrunk87@users.noreply.github.com>
2023-11-03 11:02:19 -0500

    added to webpack and js install from referencing class mini project

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,31 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      // need service work
+      // need manifest.json file
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
