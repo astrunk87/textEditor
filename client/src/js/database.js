ff49585a680d6ff97eab193d1d0b4cb79ac96b0c
astrunk87 <134554632+astrunk87@users.noreply.github.com>
2023-11-04 13:20:44 -0500

    comment changes in some files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// *done*: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => { 
   console.log('post to the database');
 
@@ -30,7 +30,7 @@ export const putDb = async (content) => {
 
 // refrenced mini project and class work 
 
-// TODO: Add logic for a method that gets all the content from the database
+// *done*: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
 
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,13 +1,13 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// *done*: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     window.defferredPrompt - event;
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// *done*: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.defferredPrompt;
 
@@ -19,7 +19,7 @@ butInstall.addEventListener('click', async () => {
     butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// *done*: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     window.defferredPrompt = null;
 });
